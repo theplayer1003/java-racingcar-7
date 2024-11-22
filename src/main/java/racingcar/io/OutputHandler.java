ce64652e7bf9d086129a7a4e18a291cc8371e105
theplayer1003 <theplayer1003@gmail.com>
2024-11-22 20:18:14 +0900

    feat(io): 결과 출력 클래스 작성

--- src/main/java/racingcar/io/OutputHandler.java
+++ src/main/java/racingcar/io/OutputHandler.java
@@ -1,4 +1,19 @@
 package racingcar.io;
 
+import java.util.Map;
+
 public class OutputHandler {
+    public static void printRoundResult(Map<String, String> result) {
+        result.entrySet().stream()
+                .forEach(entry -> {
+                    String carName = entry.getKey();
+                    String roundScore = makeRepeatScore(entry.getValue());
+                    Printer.printMessageWithFormat("%s : %s", carName, roundScore);
+                });
+    }
+
+    private static String makeRepeatScore(String score) {
+        int scoreNumber = Integer.parseInt(score);
+        return "-".repeat(scoreNumber);
+    }
 }
