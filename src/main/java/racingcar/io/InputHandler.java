5ea23eb45dba5d5dbbf249821009bcb249b27647
theplayer1003 <theplayer1003@gmail.com>
2024-11-22 19:50:28 +0900

    feat(io): 인풋 핸들러 클래스 작성

--- src/main/java/racingcar/io/InputHandler.java
+++ src/main/java/racingcar/io/InputHandler.java
@@ -1,4 +1,17 @@
 package racingcar.io;
 
 public class InputHandler {
+    public static String getCarNames() {
+        Printer.printMessage("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        Printer.printNewLine();
+
+        return Reader.getString();
+    }
+
+    public static String getRoundNumber() {
+        Printer.printMessage("시도할 횟수는 몇 회인가요?");
+        Printer.printNewLine();
+
+        return Reader.getString();
+    }
 }
